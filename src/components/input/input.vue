cad88c5f105958e8600e69f89309390a91e2229c
chengxudong <xiyang080@126.com>
2022-08-28 21:13:43 +0800

    feat: component demo

---
+++ src/components/input/input.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useAttrs } from 'vue'
+import type { FieldProps } from 'vant'
+
+interface myInputType extends Partial<FieldProps> {
+  otherProps?: boolean
+  iconName?: string
+}
+
+defineProps<myInputType>()
+
+const attrs = useAttrs()
+
+console.log('attrs', attrs)
+</script>
+
+<template>
+  <div>
+    <van-field v-bind="attrs" />
+    <slot name="icon" />
+  </div>
+</template>
